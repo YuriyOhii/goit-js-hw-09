91f0fdd09cb9ea4c350b27747158f3436b93e04e
YuriyOhii <uaogiy@gmail.com>
2023-05-22 23:10:40 +0300

    adds the first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
 
+refs.start.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  refs.stop.addEventListener('click', onStopBtnClick);
+  refs.start.disabled = true;
+  timerId = setInterval(colorBodyBG, 1000);
+}
+
+function onStopBtnClick() {
+  refs.start.disabled = false;
+  refs.stop.removeEventListener('click', onStopBtnClick);
+  clearInterval(timerId);
+}
+
+function colorBodyBG() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
