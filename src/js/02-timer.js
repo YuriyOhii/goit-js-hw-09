fb3669328e6f0f683435b62bd4a01a516e4466b3
YuriyOhii <uaogiy@gmail.com>
2023-05-23 16:40:03 +0300

    adds the second task

---
+++ src/js/02-timer.js
@@ -0,0 +1,98 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.getElementById('datetime-picker'),
+  button: document.querySelector('[data-start]'),
+};
+const dateRef = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+let periodOfTime = 0;
+let timerId = null;
+
+refs.button.disabled = true;
+
+const fp = flatpickr(refs.input, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDate) {
+    actsOnChoosenDate(selectedDate);
+  },
+  onOpen() {
+    openedCalendar();
+  },
+});
+
+function openedCalendar() {
+  clearInterval(timerId);
+  dateRef.days.textContent = '00';
+  dateRef.hours.textContent = '00';
+  dateRef.minutes.textContent = '00';
+  dateRef.seconds.textContent = '00';
+}
+
+function actsOnChoosenDate(selDate) {
+  periodOfTime = Date.parse(selDate[0]);
+  let timerValue = 0;
+
+  if (periodOfTime - new Date().getTime() > 100000) {
+    refs.button.disabled = false;
+    refs.button.addEventListener('click', onStartBtnClick, { once: true });
+  } else {
+    Notify.failure('Please choose a date in the future');
+    refs.button.disabled = true;
+  }
+}
+
+function makeFirstTimerValue() {
+  timerValue = periodOfTime - new Date().getTime();
+}
+
+function onStartBtnClick() {
+  makeFirstTimerValue();
+  timerId = setInterval(calculateAndDisplay, 1000);
+}
+
+function calculateAndDisplay() {
+  const timeObj = convertMs(timerValue);
+  displayTime(timeObj);
+  timerValue -= 1000;
+}
+
+const addLeadingZero = value =>
+    String(value).padStart(2,'0');
+
+
+
+function displayTime(value) {
+  dateRef.days.textContent = addLeadingZero(value.days);
+  dateRef.hours.textContent = value.hours;
+  dateRef.minutes.textContent = value.minutes;
+  dateRef.seconds.textContent = value.seconds;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
